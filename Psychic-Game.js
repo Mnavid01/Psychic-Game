5e60f0b9c3bdf97b500ba986ecf9485f701478ec
Silas 01 <Mnavid02@gmail.com>
2019-01-03 18:33:39 -0800

    Homework Complete

--- Psychic-Game.js
+++ Psychic-Game.js
@@ -1,4 +1,4 @@
-// Variables
+// Our Variables
 var lettersArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var winsNum = 0;
 var lossesNum = 0;
@@ -7,39 +7,39 @@ var lettersGuessedArr = [];
 var guessedLetter = null;
 var winningLetter = lettersArr[Math.floor(Math.random() * lettersArr.length)];
 
-// Creating a function to make resetting some variables easier.
+// Creating a function as to make resetting the variables easier.
 function reset() {
     guessesLeftNum = 10;
     lettersGuessedArr = [];
     winningLetter = lettersArr[Math.floor(Math.random() * lettersArr.length)];
 }
 
-// Creating a function telling the computer keystroke should be assigned to the guessedLetter variable.
+// Creating a function which tells the websites keystrokes should be assigned to a guessedLetter variable.
 document.onkeyup = function(event) {
     var guessedLetter = String.fromCharCode(event.keyCode).toLowerCase();
     
-// Tell the computer if the letter already exists or is not in the array, do nothing. Otherwise, reduce the number
-// of guesses. Not going to lie, I had to look this up...
+// Tell the website if the letter already exists or is not in the array to do nothing. Otherwise, reduce the number
+// of guesses for the player.
     if (lettersGuessedArr.indexOf(guessedLetter) < 0 && lettersArr.indexOf(guessedLetter) >= 0) {
     lettersGuessedArr[lettersGuessedArr.length]=guessedLetter;
     guessesLeftNum--;
     }
 
-// If you win, show an alert that says you win.
+// If you win, a pop-up will present an alert that says you win.
     if (winningLetter === guessedLetter) {
-        alert("The letter was " + winningLetter + "! You Win! :D");
+        alert("The letter was " + winningLetter + "! You Win! ");
         winsNum++;
         reset();
     }
 
-// If you run out of guesses, show an alert that says you lose.
+// If you run out of guesses, present an alert that says you lose.
     if (guessesLeftNum === 0) {
-        alert("You Lose! D: The letter was " + winningLetter + "!");
+        alert("You Lose! The letter was " + winningLetter + "!");
         lossesNum++;
         reset();
     }
 
-// Put our variables into our HTML to show in the DOM.
+// Put our variables into our HTML.
     $("#guesses-left").text("Guesses Left: " + guessesLeftNum);
     $("#wins").text("Wins: " + winsNum);
     $("#losses").text("Losses: " + lossesNum);
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Psychic-Game
 Psychic Game Homework For Bootcamp
+The Goal:
+I made an attempt at the Psychc Game, for the assignment I am to build a game utilizing JavaScript, in which the computer will generate a random letter, the user will have 10 chances to guess that letter in my version of the game.
+Whenever the user chooses the correct letter with-in the 10 tries, their win counter will increase by 1, and if the user is to lose, their losses counter will increase by 1.
+Everytime the user is to win or lose the game, the counter will reset without the page re-loading.
+
+What was used:
+The game was created using JavaScript
+
+Game Instructions:
+The website will randomly choose a letter, the goal is for the user to guess the letter by pressing on the keyboard.
+The user is allowed 10 chances to guess the websites number, the game will then display each incorrect guess that the player makes.
+Everytime the player guesses a letter correctly, their win count increases by one, and the screen will pull up a popup.
